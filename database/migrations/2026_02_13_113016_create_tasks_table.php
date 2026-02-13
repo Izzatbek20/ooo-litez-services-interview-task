e351210ac6e98c9d3561e168530fcbf1db51f2e7
Izzatbek20 <izzatbekmajidov200020@gmail.com>
2026-02-13 17:29:36 +0500

    feat(crm): migratsiyalar yaratildi

---
+++ database/migrations/2026_02_13_113016_create_tasks_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Modules\Crm\Enums\TaskPriorityEnum;
+use App\Modules\Crm\Enums\TaskRecurrenceEnum;
+use App\Modules\Crm\Enums\TaskRemindViaEnum;
+use App\Modules\Crm\Enums\TaskStatusEnum;
+use App\Modules\Crm\Enums\TaskTypeEnum;
+use App\Modules\Crm\Models\Task;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('client_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', TaskTypeEnum::cases());
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('priority', TaskPriorityEnum::cases());
+            $table->enum('status', TaskStatusEnum::cases())->default('pending');
+            $table->dateTime('deadline');
+            $table->boolean('is_recurring')->default(false)->comment("Vazifa tugalangandan so'ng qayta takrorlanishi");
+            $table->enum('recurrence_type', TaskRecurrenceEnum::cases())->nullable();
+            $table->integer('remind_before_minutes')->nullable()->comment("Muddatga N daqiqa qolganda eslatma yuborish.");
+            $table->enum('remind_via', TaskRemindViaEnum::cases())->nullable()->comment("Eslatma kanali");
+            $table->dateTime('reminder_sent_at')->nullable();
+            $table->dateTime('completed_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
