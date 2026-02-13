22c9c3fbe9d8c3d9865cb68b8d467e18ca6d027f
Izzatbek20 <izzatbekmajidov200020@gmail.com>
2026-02-13 17:36:25 +0500

    fix(crm): modul uchun alohida migratsiya papkasi yaratish olib tashlandi

--- app/Modules/Crm/CrmServiceProvider.php
+++ app/Modules/Crm/CrmServiceProvider.php
@@ -20,6 +20,5 @@ class CrmServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
-        $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
     }
 }
